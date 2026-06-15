7804851319eba2a6808442742ad93ea175024eeb
Shankar <thakurishankar159@gmail.com>
2026-06-15 23:07:20 +1000

    feat: add user detail controller and exclude sensitive fields from customer list queries

--- backend/src/controllers/customerController.js
+++ backend/src/controllers/customerController.js
@@ -1,3 +1,4 @@
+import { Address } from "../models/addressModel";
 import { User } from "../models/userModel.js";
 
 //get all the customers
@@ -32,7 +33,7 @@ export const getAllCustomers = async(req, res) =>{
             filter.status = status;
         }
 
-        const users = await User.find(filter).skip(skip).limit(limitNum).sort();
+        const users = await User.find(filter).select("-password -googleId -__v").skip(skip).limit(limitNum).sort();
         const total = await User.countDocuments(filter);
 
         return res.status(200).send({
@@ -60,6 +61,37 @@ export const getAllCustomers = async(req, res) =>{
 
 
 // get cutsomer details with :id
+export const getUserDetials = async(req,res) =>{
+    try {
+        const userId = req.params;
+        const user = await User.findById(userId).select("-password -googleId -__v");
+        if(!user){
+            return res.status(404).send({
+                status:"error",
+                messgae:"User not found",
+            })
+        }
+        const address = (await Address.find({user: userId})).toSorted({
+            isDefault: -1,
+            createdAt: -1,
+        })
+        return res.status(200).send({
+            status:"success",
+            message:"User details fetched successfully",
+            data:{
+                user,
+                address,
+            }
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            status:"error",
+            messgae:"Error fetching user details"
+        })
+    }
+}
 
 
 
@@ -67,6 +99,8 @@ export const getAllCustomers = async(req, res) =>{
 
 
 // update customer status like block unblock
+
+
 
 
 
