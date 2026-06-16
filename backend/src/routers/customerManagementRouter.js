002d2570ff2bda4bba2e52d7f27300e6307d6fee
Shankar <thakurishankar159@gmail.com>
2026-06-16 22:42:11 +1000

    change file name

---
+++ backend/src/routers/customerManagementRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import{getAllCustomers, getCustomerById, getOrderByUser, getOrderDetails, updateCustomerStat} from "../controllers/customerController.js"
+import {authMiddleware } from "../middlewares/authMiddleware.js";
+import {isAdmin} from "../middlewares/rolemiddleware.js";
+
+
+const router = express.Router();
+
+router.get("/", authMiddleware, isAdmin, getAllCustomers);
+router.get("/:id", authMiddleware, isAdmin, getCustomerById);
+router.get("/orders", authMiddleware, isAdmin, getOrderByUser);
+router.get("/order/:id", authMiddleware, isAdmin, getOrderDetails);
+router.patch("/status", authMiddleware, isAdmin, updateCustomerStat)
+
+export default router;
